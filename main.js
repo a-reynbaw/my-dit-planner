dd65b4970123898142ef7fc6283e772f39313f71
a-reynbaw <ammarinakou@gmail.com>
2025-07-03 01:19:37 +0300

    structure updated

--- main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-const { app, BrowserWindow } = require('electron');
-function createWindow () {
-  const win = new BrowserWindow({ width: 1000, height: 800, webPreferences: { nodeIntegration: true } });
-  win.loadFile('index.html');
-}
-app.whenReady().then(createWindow);
--- renderer.js
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-async function loadCourses() {
-  const courses = await ipcRenderer.invoke('get-courses');
-  // Render courses in your UI
-  console.log(courses);
-}
-
-loadCourses();
